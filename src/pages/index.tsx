b6f84f8d2224e623c79cbc9d9dd68a3b2fe541e6
Itzac Angelo Albertin <itzacalbertin@gmail.com>
2022-03-05 11:48:23 -0300

    added api with axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,65 @@ import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <Head>
-      <title>Pokédex</title>
-    </Head>
+    <>
+      <Head>
+        <title>Pokédex</title>
+      </Head>
+
+      <section className={styles.pageContent}>
+        <h2>Pokémons</h2>
+        <ul className={styles.pokemons}>
+          <li className={styles.pokemonContainer}>
+            <div className={styles.pokemonContent}>
+              <img src="/images/pikachu.png" alt="Pikachu" />
+              <strong>Pikachu</strong>
+            </div>
+          </li>
+          <li className={styles.pokemonContainer}>
+            <div className={styles.pokemonContent}>
+              <img src="/images/pikachu.png" alt="Pikachu" />
+              <strong>Pikachu</strong>
+            </div>
+          </li>
+          <li className={styles.pokemonContainer}>
+            <div className={styles.pokemonContent}>
+              <img src="/images/pikachu.png" alt="Pikachu" />
+              <strong>Pikachu</strong>
+            </div>
+          </li>
+          <li className={styles.pokemonContainer}>
+            <div className={styles.pokemonContent}>
+              <img src="/images/pikachu.png" alt="Pikachu" />
+              <strong>Pikachu</strong>
+            </div>
+          </li>
+          <li className={styles.pokemonContainer}>
+            <div className={styles.pokemonContent}>
+              <img src="/images/pikachu.png" alt="Pikachu" />
+              <strong>Pikachu</strong>
+            </div>
+          </li>
+          <li className={styles.pokemonContainer}>
+            <div className={styles.pokemonContent}>
+              <img src="/images/pikachu.png" alt="Pikachu" />
+              <strong>Pikachu</strong>
+            </div>
+          </li>
+          <li className={styles.pokemonContainer}>
+            <div className={styles.pokemonContent}>
+              <img src="/images/pikachu.png" alt="Pikachu" />
+              <strong>Pikachu</strong>
+            </div>
+          </li>
+          <li className={styles.pokemonContainer}>
+            <div className={styles.pokemonContent}>
+              <img src="/images/pikachu.png" alt="Pikachu" />
+              <strong>Pikachu</strong>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </>
   )
 }
 
