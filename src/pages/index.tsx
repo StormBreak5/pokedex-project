366fea3504d4162f5b43f21f979bf2309bb1e199
Itzac Angelo Albertin <itzacalbertin@gmail.com>
2022-03-07 18:26:31 -0300

    working on api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+import { api } from '../services/api'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    api.get('pokemon').then(({ data }) => {
+      setPokemons(data.results)
+    })
+
+    console.log(pokemons)
+
+  }, [])
+
   return (
     <>
       <Head>
@@ -13,54 +27,17 @@ const Home: NextPage = () => {
       <section className={styles.pageContent}>
         <h2>Pokémons</h2>
         <ul className={styles.pokemons}>
-          <li className={styles.pokemonContainer}>
-            <div className={styles.pokemonContent}>
-              <img src="/images/pikachu.png" alt="Pikachu" />
-              <strong>Pikachu</strong>
-            </div>
-          </li>
-          <li className={styles.pokemonContainer}>
-            <div className={styles.pokemonContent}>
-              <img src="/images/pikachu.png" alt="Pikachu" />
-              <strong>Pikachu</strong>
-            </div>
-          </li>
-          <li className={styles.pokemonContainer}>
-            <div className={styles.pokemonContent}>
-              <img src="/images/pikachu.png" alt="Pikachu" />
-              <strong>Pikachu</strong>
-            </div>
-          </li>
-          <li className={styles.pokemonContainer}>
-            <div className={styles.pokemonContent}>
-              <img src="/images/pikachu.png" alt="Pikachu" />
-              <strong>Pikachu</strong>
-            </div>
-          </li>
-          <li className={styles.pokemonContainer}>
-            <div className={styles.pokemonContent}>
-              <img src="/images/pikachu.png" alt="Pikachu" />
-              <strong>Pikachu</strong>
-            </div>
-          </li>
-          <li className={styles.pokemonContainer}>
-            <div className={styles.pokemonContent}>
-              <img src="/images/pikachu.png" alt="Pikachu" />
-              <strong>Pikachu</strong>
-            </div>
-          </li>
-          <li className={styles.pokemonContainer}>
-            <div className={styles.pokemonContent}>
-              <img src="/images/pikachu.png" alt="Pikachu" />
-              <strong>Pikachu</strong>
-            </div>
-          </li>
-          <li className={styles.pokemonContainer}>
-            <div className={styles.pokemonContent}>
-              <img src="/images/pikachu.png" alt="Pikachu" />
-              <strong>Pikachu</strong>
-            </div>
-          </li>
+
+          {pokemons.map(pokemon => {
+            return (
+              <li key={pokemon.id} className={styles.pokemonContainer}>
+                <div className={styles.pokemonContent}>
+                  <img src="/images/pikachu.png" alt="Pikachu" />
+                  <strong>Pikachu</strong>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </section>
     </>
